2769170d79e9cc49058fae2b9cfd962261e68f3d
Roi Kingon <rkingon@gmail.com>
2021-12-14 15:44:12 -0700

    export new modules

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { TransunionClient } from './TransunionClient';
 
-export * from './TransunionClient';
+export * from './AddonProductHandler';
+export * from './IndicativeHandler';
 export * from './Subject';
+export * from './TradeLinesHandler';
+export * from './TransunionClient';
 export * from './XMLWrapper';
 
 export default TransunionClient;
