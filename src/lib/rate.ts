b4083c8ae6fd78db0939be64407d8f1dff2ae13d
Roi Kingon <rkingon@gmail.com>
2022-01-27 16:45:09 -0700

    cleaner

--- src/lib/rate.ts
+++ src/lib/rate.ts
@@ -2,12 +2,10 @@
  * https://github.com/formulajs/formulajs/blob/master/lib/financial.js#L778
  */
 
-function monthlyRate(periods: number, payment: number, present: number, future = 0, type = 0, guess = 0.01) {
+function monthlyRate(periods: number, payment: number, present: number, future = 0, type: 0 | 1 = 0, guess = 0.01) {
 	const epsMax = 1e-10
 	const iterMax = 20
 	let rate = guess
-
-	type = type ? 1 : 0
 	for (let i = 0; i < iterMax; i++) {
 		let y
 		let f
